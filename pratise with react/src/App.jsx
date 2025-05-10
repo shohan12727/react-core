8b46affd8d082e4595af33d306fa75ec77eedf95
shohan12727 <aishohan001@gmail.com>
2025-05-11 02:34:31 +0600

    toggle done

---
+++ pratise with react/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Toggle from './Components/Practises/Toggle'
+import Toggle2 from './Components/Toggle2'
+import Toggle3 from './Components/Toggle3'
+
+function App() {
+  
+
+  return (
+    <>
+    <Toggle2></Toggle2>
+    <Toggle3></Toggle3>
+     <Toggle></Toggle>
+     
+    </>
+  )
+}
+
+export default App
---
+++ pratise with react/src/Components/Practises/Toggle.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+ 
+const Toggle = () => {
+
+const [toggle,setToggle] = useState(false)
+ const handleToggle = () => {
+    setToggle(!toggle)
+ }
+
+    return (
+        <>
+        <div style={{padding:'20px',
+            border:'2px solid red',
+            borderRadius:'20px'
+        }}>
+            <button 
+                onClick={handleToggle}
+            >
+                Pal</button>
+                {
+                    toggle && (
+                        <h1>Nondini</h1>
+                    )
+                }
+        </div>
+        </>
+    );
+};
+
+export default Toggle;
